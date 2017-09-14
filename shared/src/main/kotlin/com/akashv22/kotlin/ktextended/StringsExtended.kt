428ab7acf1d80dbcc43ad7db44b815388fa29729
AkashV22 <iakashv22@gmail.com>
2017-09-14 06:47:02 +0100

    Added the trimAll function.

--- shared/src/main/kotlin/com/akashv22/kotlin/ktextended/StringsExtended.kt
+++ shared/src/main/kotlin/com/akashv22/kotlin/ktextended/StringsExtended.kt
@@ -5,3 +5,7 @@ fun CharSequence?.isNotNullOrBlank(): Boolean = !this.isNullOrBlank()
 fun CharSequence?.isNotNullOrEmpty(): Boolean = !this.isNullOrEmpty()
 
 fun CharSequence.toStringBuilder(): StringBuilder = StringBuilder(this)
+
+fun Iterable<CharSequence>.trimAll(): List<CharSequence> {
+    return this.map { it.trim() }
+}
