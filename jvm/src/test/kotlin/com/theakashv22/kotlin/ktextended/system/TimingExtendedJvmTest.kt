4fc24e356319c57f720e3824277e5ce14c845cdc
AkashV22 <iakashv22@gmail.com>
2018-02-28 14:28:32 +0000

    Started adding tests for TimingExtended.kt.

---
+++ jvm/src/test/kotlin/com/theakashv22/kotlin/ktextended/system/TimingExtendedJvmTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 AkashV22
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * limitations under the License.
+ */
+
+package com.theakashv22.kotlin.ktextended.system
+
+import org.junit.Test
+
+/**
+ * Tests for functions in TimingExtended.kt (containing either those that are unique to the JVM or the actual
+ * declarations of functions that cannot be tested easily in the shared module)
+ */
+class TimingExtendedJvmTest {
+    /**
+     * Tests for currentTimeMillis()
+     */
+    class CurrentTimeMillisFunctionTest {
+        @Test
+        fun testFunctionReturnsCurrentTimeInMilliseconds() {
+            //TODO Implement this
+        }
+    }
+}
